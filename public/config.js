c20b4d9a87e8e58c19b1692511c3df0f3c252cc5
Weexy <146097877+Weexyy@users.noreply.github.com>
2024-04-04 22:05:27 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,6 +6,10 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
+    'm796685742-8b1d023fcc295ba6e9777eb3',
+    'm796686319-704823edefc45bc74a1af81f',
+    'ur2366472-0233f6f425e64a51f9ffccbe',
+    'ur2366472-0233f6f425e64a51f9ffccbe',
     'ur2366472-0233f6f425e64a51f9ffccbe',
   ],
 
